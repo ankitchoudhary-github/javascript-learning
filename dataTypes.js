0ce520fcdcfe1c1d51c6509b5c6fb6f370804cb6
ankitchoudhary-github <itschoudharyankit@gmail.com>
2026-07-31 15:07:03 +0530

    Practicing dataTypes

---
+++ dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";  // Treat all JS code as newer version
+
+// alert(3+3) We are using node js not browser
+console.log(3 +3) 
+console.log("Ankit");
+let age= 15;
+// Number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof age );
+console.log(typeof undefined ); // undefined
+console.log(typeof null ); // object
+
--- variable.js
+++ variable.js
@@ -2,3 +2,12 @@ const accountId = 144553
 let accountEmail= "ankit@example.com"
 var accountPassword = "12345"
 accountCity= "New Delhi"
+let accountState;
+
+// accountId= 2
+accountEmail= "abc@abc.com"
+accountPassword= "7890"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+// Var Shouldn't be allowed
+// Empty values are considered as undefined
